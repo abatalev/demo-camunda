35393a0bd326e829acff861f735882f7ddedafe1
andrey <andrey.batalev@gmail.com>
2022-12-14 12:33:50 +0300

    fix ProcessApplicationTest

--- build/process/src/test/java/com/batal/demo/camunda/ProcessApplicationTest.java
+++ build/process/src/test/java/com/batal/demo/camunda/ProcessApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProcessApplicationTests {
+class ProcessApplicationTest {
 
     @Test
     void contextLoads() {
