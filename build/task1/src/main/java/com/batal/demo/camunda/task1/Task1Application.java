4e65b5e4039a20f8d813d6a23b1f24ec7dfaddb5
andrey <andrey.batalev@gmail.com>
2024-05-16 22:38:17 +0300

    task1: exclude tracing of actuator calls

--- build/task1/src/main/java/com/batal/demo/camunda/task1/Task1Application.java
+++ build/task1/src/main/java/com/batal/demo/camunda/task1/Task1Application.java
@@ -1,11 +1,27 @@
 package com.batal.demo.camunda.task1;
 
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.contrib.sampler.RuleBasedRoutingSampler;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+import io.opentelemetry.semconv.SemanticAttributes;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Task1Application {
     public static void main(String... args) {
         SpringApplication.run(Task1Application.class);
+    }
+
+    @Bean
+    public AutoConfigurationCustomizerProvider otelCustomizer() {
+      return p ->
+          p.addSamplerCustomizer(
+              (fallback, config) ->
+                  RuleBasedRoutingSampler.builder(SpanKind.SERVER, fallback)
+                      .drop(SemanticAttributes.URL_PATH, "^/actuator")
+                      .build());
     }
 }
