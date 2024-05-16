f621ba342b47ed0aea3f9785168dea8dbc647f25
andrey <andrey.batalev@gmail.com>
2024-05-16 22:35:41 +0300

    process-mock: exclude tracing of actuator calls

--- build/process-mock/src/main/java/com/batal/demo/camunda/Application.java
+++ build/process-mock/src/main/java/com/batal/demo/camunda/Application.java
@@ -1,11 +1,14 @@
 package com.batal.demo.camunda;
 
-
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.spring.web.v3_1.SpringWebTelemetry;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.contrib.sampler.RuleBasedRoutingSampler;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+import io.opentelemetry.semconv.SemanticAttributes;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
@@ -24,5 +27,15 @@ public class Application {
         SpringWebTelemetry telemetry = SpringWebTelemetry.create(openTelemetry);
         restTemplate.getInterceptors().add(telemetry.newInterceptor());
         return restTemplate;
+    }
+
+    @Bean
+    public AutoConfigurationCustomizerProvider otelCustomizer() {
+      return p ->
+          p.addSamplerCustomizer(
+              (fallback, config) ->
+                  RuleBasedRoutingSampler.builder(SpanKind.SERVER, fallback)
+                      .drop(SemanticAttributes.URL_PATH, "^/actuator")
+                      .build());
     }
 }
