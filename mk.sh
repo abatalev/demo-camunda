b9524f59c869a198f15f2acccb8759ee20b88a89
andrey <andrey.batalev@gmail.com>
2025-01-16 11:30:29 +0300

    mk.sh: use mklib.sh

--- mk.sh
+++ mk.sh
@@ -1,118 +1,34 @@
 #!/bin/bash
-USE_CACHE=0
+
+USE_CACHE=1
+USE_COMPOSE=1
+USE_MINIKUBE=0
+USE_OPENSHIFT=0
+USE_PODMAN=0
+
+HELM_NAME=stand
+
+HELM_STATE="install"
+#HELM_STATE="delete"
 
 PRJ_IMAGEGROUP="abatalev"
 PRJ_IMAGEVERSION="2021-10-28"
-PRJ_MODULES="process process-mock task1"
+
 CDIR=$(pwd)
 
-PRJ2HASH_EXE=
-
-function check_prj2hash() {
-    if [ -f "./prj2hash" ]; then
-        PRJ2HASH_EXE="./prj2hash"
-        return 
-    fi
-    if [ -f "./tools/prj2hash" ]; then
-        PRJ2HASH_EXE="./tools/prj2hash"
-        return 
-    fi
-
-    GOLANG=$(command -v go)
-    if [ "x$GOLANG" == "x" ]; then
-        return
-    fi
-
-    if [ ! -d "${CDIR}/tools" ]; then
-        mkdir "${CDIR}/tools"
-    fi
-
-    cd "${CDIR}/tools"
-    git clone https://github.com/abatalev/prj2hash.git prj2hash.git
-    cd "${CDIR}/tools/prj2hash.git"
-    ./build.sh
-    cd "${CDIR}"
-    cp tools/prj2hash.git/prj2hash tools/prj2hash
-    rm -Rf ${CDIR}/tools/prj2hash.git/
-    PRJ2HASH_EXE="./tools/prj2hash"
-}
-
-function image_exist() {
-    I_PRJ=$1
-    V_PRJ=$2
-    docker image list | grep $I_PRJ | awk ' {print $2;}' | grep $V_PRJ | wc -l
-}
-
-function image_build() {
-    B_PRJ=$1
-    I_PRJ=$2
-    V_PRJ=$3
-    D_PRJ=$4
-    N_PRJ=$5
-    if [ ${B_PRJ} == 0 ]; then
-        echo "### build $N_PRJ"
-        docker build -t $I_PRJ:$V_PRJ -f $D_PRJ .
-        if [  $? -ne 0  ]; then exit 1; fi
-    else 
-        echo "### Build $N_PRJ skipped. Image $I_PRJ:$V_PRJ already exist"    
-    fi
-}
-
-function save_image() {
-    if [ ! -f cache/${1}-${3}.tar ]; then
-        if [ ! -d cache ]; then
-            mkdir cache
+function setup_mklib() {
+    if [ ! -f tools/mklib/mklib.sh ]; then
+        if [ ! -d tools ]; then
+            mkdir tools
         fi
-        echo "### CACHE save ${1}-${3}"
-        docker save -o cache/${1}-${3}.tar ${2}:${3}
+        cd tools
+        git clone http://localhost:3000/andrey/mklib.git
     fi
 }
 
-function load_image() {
-    BX=$(image_exist $2 $3)
-    if [ ${BX} != 1 ]; then
-        if [ -f cache/${1}-${3}.tar ]; then
-            echo "### CACHE load ${1}-${3}"
-            docker load -i cache/${1}-${3}.tar
-        fi
-    fi
-}
+setup_mklib
+cd ${CDIR}
 
-function build_project() {
-    PRJ_NAME=$1
-    PRJ_NEEDBUILD=0
-    PRJ_IMAGE="${PRJ_IMAGEGROUP}/${PRJ_NAME}"
-    PRJ_VERSION="${PRJ_IMAGEVERSION}"
-    
-    cd "${CDIR}"
-    if [  "x${PRJ2HASH_EXE}" != "x" ]; then
-        PRJ_VERSION=$(${PRJ2HASH_EXE} -short build/${PRJ_NAME})
-    fi
-    load_image ${PRJ_NAME} ${PRJ_IMAGE} ${PRJ_VERSION}
-    if [ "x${PRJ2HASH_EXE}" != "x" ]; then
-        PRJ_NEEDBUILD=$(image_exist $PRJ_IMAGE $PRJ_VERSION)
-    fi
-    cd "${CDIR}/build"
-    image_build $PRJ_NEEDBUILD $PRJ_IMAGE $PRJ_VERSION "Dockerfile.${PRJ_NAME}" "${PRJ_NAME}"
-    if [ $USE_CACHE == 1 ]; then
-        save_image ${PRJ_NAME} ${PRJ_IMAGE} ${PRJ_VERSION}
-    fi
-    cd "${CDIR}"
-    IMAGE_VERSION=$PRJ_IMAGE:$PRJ_VERSION yq e -i ".services.${PRJ_NAME}.image = strenv(IMAGE_VERSION)" docker-compose.yaml
-}
+source ${CDIR}/tools/mklib/mklib.sh
 
-function build_all() {
-    echo "### create docker-compose"
-    cp docker-compose.tmpl docker-compose.yaml
-    for prj_name in ${1}
-    do
-        build_project "${prj_name}"
-    done
-
-    cd "${CDIR}"
-    echo "### launch docker-compose"
-    docker-compose up --remove-orphans --scale process-mock=1
-}
-
-check_prj2hash
-build_all "${PRJ_MODULES}"
+build_all "process process-mock task1"
